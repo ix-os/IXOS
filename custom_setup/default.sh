66e8867f61702a160b89b1c5c3dbe9d21560c3d2
Husk <husk@ixos.com>
2020-07-30 11:59:43 +0000

    Automatic deploy

--- custom_setup/default.sh
+++ custom_setup/default.sh
@@ -1,7 +1,9 @@
-apt-get install -y nano iftop htop neofetch
+apt-get install -y nano iftop htop neofetch debian-installer debian-installer-10.0-netboot-amd64 debian-installer-launcher kexec-tools
 tasksel install desktop kde-desktop
 echo -e "\033[31mPassword for root\033[0m"
 passwd root
 adduser ix-user
-echo -e "\033[31mPassword for ix-user\033[0m"
-passwd ix-user
+echo -e "\033[31mAdd ix-user to sudoers\033[0m"
+adduser ix-user sudo
+echo "Setting wallpaper"
+python3 /etc/ksetwallpaper.py /etc/share/backdrops/ixos-wallpaper.png
