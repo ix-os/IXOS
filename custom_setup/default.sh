6470bba4f7e78f19a711428466a04d7da66cf5b7
Husk <husk@ixos.com>
2020-08-04 10:57:07 +0000

    Automatic deploy

--- custom_setup/default.sh
+++ custom_setup/default.sh
@@ -1,4 +1,7 @@
-apt-get install -y nano iftop htop neofetch debian-installer debian-installer-launcher kexec-tools
+apt-get install -y nano iftop htop neofetch debian-installer debian-installer-launcher kexec-tools 
+dpkg --add-architecture i386 && apt update
+echo -e "\033[31mInstalling wine and deps\033[0m"
+apt install -y      wine       wine32       wine64       libwine       libwine:i386       fonts-wine
 tasksel install desktop kde-desktop
 echo -e "\033[31mPassword for root\033[0m"
 passwd root
