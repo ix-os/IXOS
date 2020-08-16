0bca3b0f03a1da170dce3e8e84dba88668d7abb5
Husk <husk@ixos.com>
2020-08-16 10:32:33 +0000

    Automatic deploy

--- custom_setup/default.sh
+++ custom_setup/default.sh
@@ -1,7 +1,7 @@
-apt-get install -y nano iftop htop neofetch debian-installer debian-installer-launcher kexec-tools  open-vm-tools wget gnupg gnupg2 gnupg1
+apt-get install -y nano iftop htop neofetch debian-installer debian-installer-launcher kexec-tools wget gnupg gnupg2 gnupg1
 dpkg --add-architecture i386 && apt update
 echo -e "\033[31mInstalling wine and deps\033[0m"
-apt install -y wine wine32 wine64 libwine libwine:i386 fonts-wine
+apt install -y wine wine32 wine64 libwine libwine:i386 fonts-wine libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386
 echo -e "\033[31mInstalling Lutris\033[0m"
 echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
 wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -
